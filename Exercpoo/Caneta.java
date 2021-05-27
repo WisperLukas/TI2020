24a45ab9826ff3354d57a33b39da8039f81f66b8
Whisper <pedrolucaspupy@gmail.com>
2021-05-27 17:42:53 +0000

    commit on 'Exercicios'

--- Exercpoo/Caneta.java
+++ Exercpoo/Caneta.java
@@ -6,16 +6,28 @@ public class Caneta {
     float ponta;
     int carga;
     boolean tampada;
+    
+    void status(){
+        System.out.println("Modelo "+ this.modelo);
+        System.out.println("Ponta: "+ this.ponta);
+        System.out.println("Uma caneta "+ this.cor);
+        System.out.println("Carga "+ this.carga);
+        System.out.println("Esta tampada ? "+ this.tampada);
+    } 
 
     void rabiscar(){
-
+if (this.tampada == true){
+    System.out.println("ERRO! não posso rabiscar com a caneta tampada.");
+}else{
+    System.out.println("Rabiscando.....");
+}
 
     }
     void tampar(){
-
+this.tampada = true;
     
     }
     void destampar(){
-        
+        this.tampada = false;
     }
 }
--- Exercpoo/Java1.java
+++ Exercpoo/Java1.java
@@ -2,5 +2,13 @@ package Exercpoo;
 
 public class Java1{
 public static void main(String[] args) {
+    Caneta c1 = new Caneta();
+    c1.cor = "azul";
+    c1.ponta = 0.5f;
+    c1.tampar();
+    c1.rabiscar();
+    c1.status();
+
+
     
 }}
